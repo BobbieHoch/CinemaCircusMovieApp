6893f4a9c475ec20cce2b767489e3bf1b4433749
David <dpatterson1995@yahoo.com>
2023-02-13 21:59:53 -0500

    added some things

--- cinema/src/Componets/Header.tsx
+++ cinema/src/Componets/Header.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 export function Header () {
   return (
     <div className="Header">
-      
+      <h1>Cinema Circus</h1>
+      <p>A Resource For All Your Movie Needs!</p>
     </div>
   );
 }
--- cinema/src/Componets/SearchBar.tsx
+++ cinema/src/Componets/SearchBar.tsx
@@ -3,7 +3,8 @@ import * as React from 'react';
 export function SearchBar () {
   return (
     <div className="SearchBar">
-      
+      <input type="text" placeholder='Search For A Movie' />
+      <button type="submit">Submit</button>
     </div>
   );
 }
