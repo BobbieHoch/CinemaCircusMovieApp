c1755f7f4df5880cb330b7552e84c67232fec436
Melissa Morley <melissarose.morley@gmail.com>
2023-02-16 21:12:55 -0500

    fixed card images

--- cinema/src/Componets/MovieList.tsx
+++ cinema/src/Componets/MovieList.tsx
@@ -16,44 +16,9 @@ export function MovieList(){
 
   return(
     <div className = "MovieList">
-      {setMovieResponse !== undefined && movieResponse?.results.map((movie) => <MovieCard movie = {movie}/>)}
+      {setMovieResponse !== undefined && movieResponse?.results.map((movie) => <MovieCard movie = {movie} key = {movie.id}/>)}
     </div>
   )
 }
 
 
-
-
-// export function MovieList () {
-//   const [moviesResponse, setMovieResponse] = useState<MovieResponse>();
-//   useEffect(() => {
-//     GetMovieData().then(data => setMovieResponse(data))
-//   }, []);
-
-// useEffect(() => {
-//   console.log(moviesResponse);
-// }, [moviesResponse]);
-
-
-// let results = moviesResponse?.results;
-
-// // let baseImageURL = "https://image.tmdb.org/t/p/original"
-  
-// // let displayResults = results?.map((movie) =>
-// // <ul>
-// //   <li>{baseImageURL}{movie.poster_path}</li>
-// //   <li>{movie.title}</li>
-// //   <li>{movie.overview}</li>
-// //   <li>Genre</li>
-// //   <li>Rating{movie.vote_average}</li>
-// // </ul>
-// // )
-
-
-//   return (
-//     <div className="MovieList">
-//       <div>MovieList</div>
-//       {moviesResponse !== undefined && moviesResponse.results.map((movie) => <MovieCard movie = {movie}/>)}
-//     </div>
-//   );
-// }
