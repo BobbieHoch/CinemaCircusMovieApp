d0bc6b7b41294b04093bd3fba15f683bf060753a
BobbieHoch <rwhochstadt@gmail.com>
2023-02-20 20:06:14 -0500

    apicallsthree

--- cinema/src/Services/MovieService.tsx
+++ cinema/src/Services/MovieService.tsx
@@ -1,10 +1,22 @@
-import { Movie } from "../models/Movie";
 import axios from "axios";
-import { MovieResponse } from "../models/MovieTest";
 
-export function GetMovieData(): Promise<MovieResponse>  {
-    return axios
+export async function searchMoviesByKeyword(): Promise<Result> {
+    const response = await axios.get(
+        "https://api.themoviedb.org/3/search/keyword?api_key=52347ae3c2afc9c57260e9eff233ad3b&language=en-US&page=1&include_adult=false"
+    );
+    return response.data.results;
+}
 
-    .get<MovieResponse>("https://api.themoviedb.org/3/movie/popular?api_key=52347ae3c2afc9c57260e9eff233ad3b&language=en-US&page=1")
-    .then((response) => response.data);
+export async function getMoviesByGenre(): Promise<Result> {
+    const response = await axios.get(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=52347ae3c2afc9c57260e9eff233ad3b&language=en-US&page=1&include_adult=false"
+    );
+    return response.data.results;
+}
+
+export async function getMoviesByRating(): Promise<Result> {
+    const response = await axios.get(
+        "https://api.themoviedb.org/3/search/vote_average?api_key=52347ae3c2afc9c57260e9eff233ad3anguage=en-US&page=1&include_adult=false"
+    );
+    return response.data.results;
 }
